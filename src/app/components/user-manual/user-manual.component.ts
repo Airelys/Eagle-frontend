655413cb55b0452bdd19fd1023c6e999168ee603
Airelys <Airelys@DESKTOP-PNPOLM3>
2022-11-25 00:51:41 -0500

    arreglando manual de usuario

--- src/app/components/user-manual/user-manual.component.ts
+++ src/app/components/user-manual/user-manual.component.ts
@@ -7,9 +7,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserManualComponent implements OnInit {
 
+  met_num = false;
+  met_clas = false;
+  met = false;
+  esp = false;
+  xlsx = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  MetNum():void{
+    this.met_num = true;
+    this.met_clas = false;
+    this.met = false;
+    this.esp = false;
+    this.xlsx = false;
+  }
+
+  MetClas():void{
+    this.met_num = false;
+    this.met_clas = true;
+    this.met = false;
+    this.esp = false;
+    this.xlsx = false;
+  }
+
+  Met():void{
+    this.met_num = false;
+    this.met_clas = false;
+    this.met = true;
+    this.esp = false;
+    this.xlsx = false;
+  }
+
+  Esp():void{
+    this.met_num = false;
+    this.met_clas = false;
+    this.met = false;
+    this.esp = true;
+    this.xlsx = false;
+  }
+
+  Xlsx():void{
+    this.met_num = false;
+    this.met_clas = false;
+    this.met = false;
+    this.esp = false;
+    this.xlsx = true;
+  }
+
+  closed():void{
+    this.met_num = false;
+    this.met_clas = false;
+    this.met = false;
+    this.esp = false;
+    this.xlsx = false;
   }
 
 }
